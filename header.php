0cec5632dc605929be754309509a30f9add1e355
louisaferdinand <louisaferdinand@itbss.ac.id>
2026-06-01 17:37:53 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,17 @@
+<?php
+include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Kelas Kopi Online</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<header>
+    <h1>Kelas Kopi Online</h1>
+    <p>Belajar menyeduh kopi dan membangun bisnis kopi rumahan</p>
+</header>
